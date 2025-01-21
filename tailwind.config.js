f299446b2914b8bd4c754a20f2467c04bed076c3
Q-Lukyss <quentin.wow@free.fr>
2025-01-21 14:17:31 +0100

    ajout police et couleurs tailwind scss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "dark-blue": "#262733",
+        "vibrant-orange": "#e64a3b",
+        "papyrus": "#eee2d4",
+        "alternate-gray-blue": "#3b3c47",
+        "mygray": "#a5a5aa",
+      },
+      fontFamily: {
+        squarePig: ['Square Pig', 'sans-serif'], 
+      },
+    },
   },
   plugins: [],
 }
